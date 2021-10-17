71d0642fe1261f0e2ff28b6768b877ee950bbf2d
Dana Sugu <dana.sugu@gmail.com>
2021-10-17 10:53:30 +0300

    Update Pages.php

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,8 +1,15 @@
 <?php
 
 class Pages extends CI_Controller {
-  public  function view($page = 'home')
+  public function view($page = 'home')
   {
-    
+    if(!page_exists(APPPATH. 'views/pages/' . $page .'.php'))
+   {
+      show_404();
+    }
+    $data ['$title'] = ucfirst($page);
+    $this->load->view('templates/header');
+    $this->load->view('pages/' .$page, $data);
+    $this->load->view('templates/footer');
   }
 }
