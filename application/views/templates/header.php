008cbda94c2a5eaa9b0d785bc1a643176eda4710
Dana Sugu <dana.sugu@gmail.com>
2021-10-17 14:26:18 +0300

    update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,5 +5,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ciBlog</title>
+  <link rel="stylesheet" href="<?php echo base_url('/assets/css/bootstarp.min.css')?>">
 </head>
 <body>
