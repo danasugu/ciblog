f3f21eef71f3ad78260850d019766bffe5b92eb2
Dana Sugu <dana.sugu@gmail.com>
2021-10-18 08:14:19 +0300

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,20 +6,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ciBlog</title>
   <link rel="stylesheet" href="<?php echo base_url('/assets/css/bootstrap.min.css')?>">
+   <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
 
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
- <div id="navbar">
-      <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-    </ul>
- </div>
-  </div>
-</nav>
+	<nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">ciBlog</a>
+        </div>
+        <div id="navbar">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+             <li><a href="#posts">Blog</a></li>
+             <li><a href="#categories">Categories</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </nav>
