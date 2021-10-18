cdd4fa9bfc2801f331a776777d3b1a53223c8a75
Dana Sugu <dana.sugu@gmail.com>
2021-10-18 07:59:44 +0300

    update

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,16 +1,1 @@
 <h2><?= $title ?></h2>
-<nav class="navbar navbar-inverse">
-  <div class="container">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
- <div id="navbar">
-      <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
-    </ul>
- </div>
-  </div>
-</nav>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,3 +9,18 @@
 </head>
 <body>
 
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+ <div id="navbar">
+      <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <!-- <li><a href="#">Page 3</a></li> -->
+    </ul>
+ </div>
+  </div>
+</nav>
